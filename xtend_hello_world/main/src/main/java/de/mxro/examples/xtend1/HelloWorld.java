b1bb0ba1f999e1dccfcf52580fc087835c1e023f
GDwag <gilbert@groten.fr>
2019-06-19 16:53:00 +0200

    xtend hello world

---
+++ xtend_hello_world/main/src/main/java/de/mxro/examples/xtend1/HelloWorld.java
@@ -0,0 +1,9 @@
+package de.mxro.examples.xtend1;
+
+@SuppressWarnings("all")
+public class HelloWorld {
+  public void helloWorld() {
+    final String message = "Hello, World!";
+    System.out.println("\'«message»\'");
+  }
+}
---
+++ xtend_hello_world/main/xtext/de/mxro/examples/xtend1/HelloWorld.xtend
@@ -0,0 +1,10 @@
+package de.mxro.examples.xtend1
+
+class HelloWorld {
+	def public void helloWorld() {
+
+        val message = "Hello, World!";
+
+		System::out.println("'«message»'")
+    }
+}
